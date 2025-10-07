7595d4057d27b812783d9f2ae9ede7607f2fb292
samuelslv <samuelslv7@gmail.com>
2025-10-07 20:46:05 -0300

    modulo2-index1

---
+++ modulo2-HTML_CSS3/index2.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title> Meu primeiro site em PHP </title>
+    </head>
+
+    <body>
+        <?php
+            for($i = 0 ; $i < 10 ; $i++){
+                print("Contador: " . $i + 1 . "<br/>");
+            }
+            print("FIm de contagem")
+        ?>
+
+    </body>
+
+</html>
